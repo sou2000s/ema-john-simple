1a5b1902a9453071affbe44f6044ffa7a448b673
netox222 <souparnamahato146@gmail.com>
2022-11-02 20:39:25 +0530

    first

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,3 +1,23 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyABOTLhorghUSE65XowCLOGNfDULmJoXM4",
+//   authDomain: "ema-john-simple-a7a92.firebaseapp.com",
+//   projectId: "ema-john-simple-a7a92",
+//   storageBucket: "ema-john-simple-a7a92.appspot.com",
+//   messagingSenderId: "853262808476",
+//   appId: "1:853262808476:web:4e7ee600823a81b130c459"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// 
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,14 +25,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABOTLhorghUSE65XowCLOGNfDULmJoXM4",
-  authDomain: "ema-john-simple-a7a92.firebaseapp.com",
-  projectId: "ema-john-simple-a7a92",
-  storageBucket: "ema-john-simple-a7a92.appspot.com",
-  messagingSenderId: "853262808476",
-  appId: "1:853262808476:web:4e7ee600823a81b130c459"
+  apiKey: "AIzaSyCw_55hxRs4Zl1u_wAiHMETC9TU-rnvP3k",
+  authDomain: "ema-jhon-auth-60f11.firebaseapp.com",
+  projectId: "ema-jhon-auth-60f11",
+  storageBucket: "ema-jhon-auth-60f11.appspot.com",
+  messagingSenderId: "993291061709",
+  appId: "1:993291061709:web:36ed4eb4f3c86885ff5c84"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
